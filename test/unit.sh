8c33439ab174dbcf93d1bc20f0b37e2860fc058a
Peter Aba <peter@peteraba.com>
2015-05-14 13:22:38 +0200

    Adding german/util to unit test runner

--- test/unit.sh
+++ test/unit.sh
@@ -6,5 +6,8 @@ echo ""
 go test github.com/peteraba/d5/lib/german/dict -cover
 echo ""
 
+go test github.com/peteraba/d5/lib/german/util -cover
+echo ""
+
 go test github.com/peteraba/d5/lib/util -cover
 echo ""
