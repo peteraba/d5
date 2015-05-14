442a24e4b387b0d7be60c7b3ee70a9d15bf16126
Peter Aba <peter@peteraba.com>
2015-05-14 13:59:43 +0200

    Making unit tests added automatically to suite

--- test/unit.sh
+++ test/unit.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
-go test github.com/peteraba/d5/lib/german/entity -cover
-echo ""
-
-go test github.com/peteraba/d5/lib/german/dict -cover
-echo ""
-
-go test github.com/peteraba/d5/lib/german/util -cover
-echo ""
-
-go test github.com/peteraba/d5/lib/util -cover
-echo ""
+for f in $(find ../lib -type d -printf '%d\t%P\n' | sort -r -nk1 | cut -f2-); 
+do 
+	echo $(go test github.com/peteraba/d5/lib/$f -cover)
+	echo ""
+done
