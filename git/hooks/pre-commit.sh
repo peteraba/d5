f9297acb6a093eef4027d32fb28e0bdc53d646f7
Peter Aba <peter@peteraba.com>
2015-05-19 20:47:26 +0200

    Moving integration tests into pre-push hook

--- git/hooks/pre-commit.sh
+++ git/hooks/pre-commit.sh
@@ -15,15 +15,3 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-
-echo ""
-echo ""
-print_title  "Integration tests"
-print_output "================="
-echo ""
-../../test/integration.sh
-if [[ $? -ne 0 ]]; then
-	test_error
-	print_error "Integration tests failed."
-	exit 1
-fi
